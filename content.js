165473705498091c86dae6b51c3658314d88f543
Serena Roderick <78286589+serenaroderick@users.noreply.github.com>
2023-07-15 17:14:33 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 const addButton = document.getElementById("addButton");
-addButton.addEventListener("click", addEntry);
+//addButton.style.backgroundImage = "url('images/add.png)";
+addButton.addEventListener("click", addClicked);
+
+// const searchButton = document.getElementById("searchButton");
+// searchButton.addEventListener("click", searchClicked);
 
 var numEntries = 0;
 
@@ -7,20 +11,25 @@ populateFromSync();
 chrome.storage.sync.clear();
 
 function populateFromSync() {
-    //chrome.storage.sync.clear();
+    // chrome.storage.sync.clear();
     chrome.storage.sync.get(null, (items) => {
         var allKeys = Object.keys(items);
         var allValues = Object.values(items);
+        console.log('FROM SYNC ( KEY | VALUE )')
         for(var i = 0; i < allKeys.length; i++) {
-            console.log(`key: ${allKeys[i]} value: ${allValues[i]}`);
+            console.log(`${allKeys[i]}     |      ${allValues[i]}`);
             createEntry(allKeys[i], allValues[i]);
         }
+        console.log('____________')
     });
 }
 
-function addEntry() {
+function addClicked() {
     var keyField = document.getElementById("keyField").value;
     var valueField = document.getElementById("valueField").value;
+    if (!valueField) {
+        return;
+    }
     document.getElementById("keyField").value = "";
     document.getElementById("valueField").value = "";
     createEntry(keyField, valueField);
@@ -28,43 +37,71 @@ function addEntry() {
 
 function createEntry(key, value){
     numEntries++;
-    var entryRow = document.getElementById("entryRow");
+    var entries = document.getElementById("entries");
+    var entry = document.createElement("div");
+    entry.setAttribute("class", "entry");
+    entry.setAttribute("id",numEntries);
+    entries.appendChild(entry);
 
     var keyElement = document.createElement("p");
     keyElement.innerHTML = key;
-    entryRow.appendChild(keyElement);
-    keyElement.setAttribute("class", "keyEntry");
-    keyElement.setAttribute("id", numEntries);
+    entry.appendChild(keyElement);
+    keyElement.setAttribute("class", "entryKey");
 
     var valueElement = document.createElement("input");
     valueElement.value = value;
-    entryRow.appendChild(valueElement);
-    valueElement.setAttribute("class", "valueEntry");
-    valueElement.setAttribute("id", numEntries);
+    entry.appendChild(valueElement);
+    valueElement.setAttribute("class", "entryValue");
 
     var copyButton = document.createElement("button");
-    copyButton.innerHTML = "Copy";
-    entryRow.appendChild(copyButton);
+    copyButton.style.backgroundImage = "url('images/copy.png')";
     copyButton.setAttribute("class", "copyButton");
-    copyButton.setAttribute("id", numEntries);
     copyButton.addEventListener("click", copyText);
-    storeEntry(key, value);
+    entry.appendChild(copyButton);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.style.backgroundImage = "url('images/trash.png')";
+    entry.appendChild(deleteButton);
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.addEventListener("click", deleteEntry);
+    
+    storeEntryToSync(key, value);
 }
 
 function copyText() {
-    var clickedId = this.id;
-    var copyText = "";
-    var valueArray = document.getElementsByClassName("valueEntry");
-    for(i = 0; i < valueArray.length; i++) {
-        if (valueArray[i].id == clickedId) copyText = valueArray[i].value;
+    var entry = this.parentNode;
+    var valElement = entry.getElementsByClassName("entryValue")[0];
+    var copyText = valElement.value;
+    copyElements = document.getElementsByClassName("copyButton");
+    for (var  i = 0; i < copyElements.length; i++) {
+        copyElements[i].style.backgroundColor = "black";
     }
-    this.style.backgroundColor = "darkgreen";
+    this.style.backgroundColor = "#023c0f";
     navigator.clipboard.writeText(copyText);
+    //this.style.backgroundColor = "black";
 }
 
 
-function storeEntry(keyString, valueString) {
-    chrome.storage.sync.set({[keyString]: valueString}, function() {
-        console.log(`key set to ${keyString}`);
+function storeEntryToSync(keyString, valueString) {
+    chrome.storage.sync.set({[keyString]: valueString});
+}
+
+function deleteEntry() {
+    console.log('DELETE');
+    var entry = this.parentNode;
+    var key = entry.getElementsByClassName('entryKey')[0].innerHTML;
+    chrome.storage.sync.remove(key);
+    this.parentNode.remove();
+}
+
+function searchClicked() {
+    var targetString = document.getElementById("searchbar").value;
+    console.log(targetString);
+    chrome.storage.sync.get(null, (items) => {
+        var allKeys = Object.keys(items);
+        var allValues = Object.values(items);
+        for(var i = 0; i < allKeys.length; i++) {
+            
+        }
     });
 }
